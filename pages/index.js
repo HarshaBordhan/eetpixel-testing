5eb732e237db5ab21d0c33f625003ca8d93a9b90
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-03-13 00:16:06 +0600

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -117,7 +117,7 @@ export default function Home() {
                 </div>
               </div>
             </section>
-            <section className="flex flex-col justify-center gap-8 mb-44">
+            <section className="flex flex-col justify-center gap-8 mb-44 pt-20">
               <div className="flex flex-col items-center gap-3">
                 {" "}
                 <span>
