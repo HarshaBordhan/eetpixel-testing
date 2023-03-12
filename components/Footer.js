24e6aad48da95f0356bf9e2e93c44654dbb44352
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-03-13 00:20:27 +0600

    Initial Commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     // <footer className="h-10 text-center">
-    <footer className="h-10 text-center">
+    <footer className="text-center">
       <p className="font-thin text-sm">
         Copyright &#169; 2022. All rights reserved by eetpixel LLC
       </p>
