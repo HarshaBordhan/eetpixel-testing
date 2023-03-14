d8017b40e5bf806978e8ec599f2f9591548b134b
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-03-14 23:38:01 +0600

    Initial Commit

--- components/Header.js
+++ components/Header.js
@@ -33,7 +33,9 @@ export default function Header() {
         </div>
         <nav className="nav__links">
           <ul className="flex flex-row justify-between items-center h-12 px-2 py-1 rounded-full bg-[#1a1c23]">
-            <li className="nav-items activeNav">Work</li>
+            <li className="nav-items activeNav">
+              <Link>Work</Link>
+            </li>
             <li className="nav-items">Services</li>
             <li className="nav-items">About</li>
             <li className="nav-items">Contact us</li>
