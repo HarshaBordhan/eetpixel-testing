aef201466679c7d7a870490278bc9eddca9e6acf
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-03-19 02:23:44 +0600

    Initial Commit

--- pages/jigstack.js
+++ pages/jigstack.js
@@ -1,7 +1,61 @@
 import Link from "next/link";
 
 export default function Jigstack() {
-  return (<main>
-    <Link href="/"></Link>
-  </main>);
+  return (
+    <main>
+      <Link href="/">
+        <svg
+          width="87"
+          height="32"
+          viewBox="0 0 87 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clip-path="url(#clip0_1286_46)">
+            <path
+              d="M43.5 0H3C3.945 0 17.5 0.225 25.14 10.76C33.04 21.68 34.7 29 43.5 29C52.3 29 53.96 21.68 61.86 10.76C69.5 0.225 83.055 0 84 0H43.5Z"
+              fill="white"
+            />
+            <path
+              d="M43.5 0H3C3.945 0 17.5 0.225 25.14 10.76C33.04 21.68 34.7 29 43.5 29C52.3 29 53.96 21.68 61.86 10.76C69.5 0.225 83.055 0 84 0H43.5Z"
+              fill="white"
+            />
+            <path
+              d="M43.5 0H0C1.025 0 15.59 0.25 23.78 11.875C32.265 23.925 34.05 32 43.5 32C52.95 32 54.76 23.925 63.22 11.875C71.41 0.25 86 0 87 0H43.5Z"
+              fill="white"
+            />
+            <g clip-path="url(#clip1_1286_46)">
+              <path
+                d="M48.5 11.5L39.5 20.5"
+                stroke="black"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M39.5 11.5L48.5 20.5"
+                stroke="black"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </g>
+          <defs>
+            <clipPath id="clip0_1286_46">
+              <rect width="87" height="32" fill="white" />
+            </clipPath>
+            <clipPath id="clip1_1286_46">
+              <rect
+                width="18"
+                height="18"
+                fill="white"
+                transform="translate(35 7)"
+              />
+            </clipPath>
+          </defs>
+        </svg>
+      </Link>
+    </main>
+  );
 }
