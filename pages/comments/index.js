41c5a57f180f76c19891f1dc0a752bef1d61817f
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-03-25 20:33:06 +0600

    Initial Commit

---
+++ pages/comments/index.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function CommentList() {
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
+
+  const fetchComments = async () => {
+    const res = await fetch("/api/comments");
+    const data = await res.json();
+    setComments(data);
+  };
+
+  const submitComments = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/comments", {
+      method: "POST",
+      // body: JSON.stringify({ comment: comment }),
+      body: JSON.stringify({ comment }), // the 'comment' need to extract in api routes
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
+  return (
+    <>
+      <input
+        className="text-black"
+        type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        autoComplete="off"
+      />
+      <button onClick={submitComments}>Submit comments</button>
+      <button onClick={fetchComments}>Load comments</button>
+      {comments.map((comment) => {
+        return (
+          <div key={comment.id}>
+            {comment.id} {comment.text}
+          </div>
+        );
+      })}
+    </>
+  );
+}
