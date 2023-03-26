d3d4b6f115b5c0ba48b47259a5aa8849f6894322
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-03-27 00:37:42 +0600

    Initial Commit

--- pages/person/index.js
+++ pages/person/index.js
@@ -1,8 +1,33 @@
 // import { useSWR } from "swr";
 
+import { useEffect, useState } from "react";
+
 // const fetcher = () => {};
 
 export default function Person() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [personData, setPersonData] = useState(null);
   // const { data, error } = useSWR("person", fetcher);
-  return <></>;
+
+  useEffect(() => {
+    const fetchData = async function () {
+      const res = await fetch("/api/people");
+      const data = await res.json();
+      setPersonData(data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <div className="text-white">
+        {personData.id} {personData.name}
+      </div>
+    </>
+  );
 }
